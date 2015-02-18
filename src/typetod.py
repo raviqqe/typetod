1a9839e0e20188048e0f4e0af663f6491cc2c021
raviqqe <raviqqe@gmail.com>
2015-02-18 22:32:44 +0000

    extra empty lines omitted

--- src/typetod.py
+++ src/typetod.py
@@ -609,13 +609,13 @@ try:
           window.addstr(0, 0, "you survived!")
         else:
           window.addstr(0, 0, "you did it!")
-        window.addstr(2, 0, "{:9s} {:>8s}"
+        window.addstr(1, 0, "{:9s} {:>8s}"
             .format('speed:', game.get_speed()))
-        window.addstr(3, 0, "{:9s} {:>6s}"
+        window.addstr(2, 0, "{:9s} {:>6s}"
             .format('accuracy:', game.get_accuracy()))
-        window.addstr(4, 0, "{:9s} {:>5s}"
+        window.addstr(3, 0, "{:9s} {:>5s}"
             .format('typos:', game.get_errors()))
-        window.addstr(6, 0, "press any key...")
+        window.addstr(4, 0, "press any key...")
         window.getch()
         screen = Screen.leave
   
