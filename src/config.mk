8574af499d47ba075fefb1b11d532ce825e943dc
raviqqe <raviqqe@gmail.com>
2015-04-02 11:14:41 +0000

    MANPREFIX variable in config.mk removed

--- src/config.mk
+++ src/config.mk
@@ -1,7 +1,6 @@
 # change these parameters to fit it to your system
 
 PREFIX ?= /usr/local
-MANPREFIX ?= /usr/local
 
 BINDIR = ${DESTDIR}${PREFIX}/bin
-MANDIR = ${DESTDIR}${MANPREFIX}/man/man6
+MANDIR = ${DESTDIR}${PREFIX}/man/man6
